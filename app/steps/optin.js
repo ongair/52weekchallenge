fb642b433e71255d892993b2f9483b5e330d38cf
Trevor Kimenye <kimenye@gmail.com>
2017-04-22 11:31:58 +0200

    Added intro text

--- app/steps/optin.js
+++ app/steps/optin.js
@@ -15,8 +15,14 @@ class OptIn extends YesNoStep {
 
       if (yes) {
         key = 'intro'
-        messages.push(new Message(user, "Great! Here's the gist of the challenge."))
-        messages.push(new Message(user, "Start with a small amount to save at the start of the year then double this amount each week"))
+        messages.push(new Message(user, "Great! Let me show you how."))
+        messages.push(new Message(user, "First start with a small amount, like Ksh. 50. Then each week increase the amount you save by this same amount."))
+        messages.push(new Message(user, "So you save 100 in week 2, 150 in week 3 and so on until 2,600 in the last week of the year."))
+        messages.push(new Message(user, "By then you will have saved Ksh. 68,900!", ["Huh?", "Let's do this"]))
+      }
+      else {
+        key = 'new'
+        messages.push(new Message(user, "No worries. Should you change your mind let me know", ["Save"]))
       }
 
       resolve({ key: key, messages: messages })
