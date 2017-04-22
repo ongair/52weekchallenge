8473ba400d02891b8ead82bfb2a9167b634f7792
Trevor Kimenye <kimenye@gmail.com>
2017-04-22 10:57:28 +0200

    Added calculator

--- app/lib/calculator.js
+++ app/lib/calculator.js
@@ -1,15 +1,26 @@
 class Calculator {
 
-  constructor(installment=100, start=0) {
-    this.installment = installment
+  constructor(seed=100, start=1) {
+    this.seed = seed
     this.start = start
+  }
+
+  balance(week) {
+    let total = 0
+    for(let idx=1; idx <= week; idx++)
+      total += this.installment(idx)
+    return total
+  }
+
+  installment(week) {
+    return week * this.seed
   }
 
   static weekFromDate(date) {
     let firstJanOfYear = new Date(date.getFullYear(),0,1)
     let millisecsInDay = 86400000
 
-    return Math.ceil(((( date - firstJanOfYear ) / millisecsInDay ) + firstJanOfYear.getDay() + 1 ) / 7)    
+    return Math.ceil(((( date - firstJanOfYear ) / millisecsInDay ) + firstJanOfYear.getDay() + 1 ) / 7)
   }
 }
 
--- test/lib/calculator.test.js
+++ test/lib/calculator.test.js
@@ -22,4 +22,35 @@ describe('The calculator', () => {
     week = Calculator.weekFromDate(date)
     expect(week).to.be.equal(53) //next year for 2017
   })
+
+  it('Can the installment for a particular week', () => {
+
+    let calc = new Calculator(50)
+
+    let amount = calc.installment(1)
+    expect(amount).to.be.equal(50)
+
+    amount = calc.installment(2)
+    expect(amount).to.be.equal(100)
+
+    amount = calc.installment(3)
+    expect(amount).to.be.equal(150)
+
+    amount = calc.installment(52)
+    expect(amount).to.be.equal(2600)
+  })
+
+  it('Can get the balance for a particular week', () => {
+
+    let calc = new Calculator(50)
+
+    let balance = calc.balance(1)
+    expect(balance).to.be.equal(50)
+
+    balance = calc.balance(2)
+    expect(balance).to.be.equal(150)
+
+    balance = calc.balance(52)
+    expect(balance).to.be.equal(68900)
+  })
 })
